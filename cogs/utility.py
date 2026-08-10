12734c2e64c9c07d3c69dc2f95233dc6d08c1e92
Akko <maple@akkos2.anonaddy.com>
2026-08-10 17:53:42 +0000

    2026-08-10

---
+++ cogs/utility.py
@@ -0,0 +1,37 @@
+# Bibliotecas
+from workers.database import guild_settings_get
+from utils.localization import get_language
+from discord.ext import commands
+from discord import app_commands
+import discord
+
+
+# Definição classe Cog
+class Utility(commands.Cog):
+    def __init__(self, yume: commands.AutoShardedBot):
+        self.yume = yume
+
+    # Avisa quando o cog iniciar
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Utility is Ready!')
+
+    # Ping
+    @app_commands.command(name="ping", description="You can use this to see my current latency.")
+    async def ping(self, interaction: discord.Interaction):
+
+        guild_id = interaction.guild_id if interaction.guild_id else 0
+        lang, persona = await guild_settings_get(guild_id)
+
+        latency = round(self.yume.latency * 1000)
+
+        get_phrase = get_language(lang, "", "CommandPing", persona)
+        phrase = get_phrase.format(ping = latency)
+
+        await interaction.response.send_message(phrase)
+
+
+
+# Registra a classe nos Cogs
+async def setup(yume: commands.AutoShardedBot) -> None:
+    await yume.add_cog(Utility(yume))
