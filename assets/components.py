466a6530d241c1b01ce69ca7faef9b1b10c9d721
Akko <maple@akkos2.anonaddy.com>
2026-08-20 04:21:29 +0000

    2026-08-19²

--- assets/components.py
+++ assets/components.py
@@ -12,27 +12,16 @@ class BannerComponent(discord.ui.LayoutView):
         ]
 
         if global_banner is not None:
-            components.append(
-                discord.ui.MediaGallery(
-                    discord.MediaGalleryItem(media = global_banner)
-                )
-            )
+            components.append(discord.ui.MediaGallery(discord.MediaGalleryItem(media = global_banner)))
 
         if global_banner is not None and server_banner is not None:
-            components.append(
-                discord.ui.Separator(visible = True, spacing = discord.SeparatorSpacing.small)
-            )
+            components.append(discord.ui.Separator(visible = True, spacing = discord.SeparatorSpacing.small))
 
         if server_banner is not None:
-            components.append(
-                discord.ui.MediaGallery(
-                    discord.MediaGalleryItem(media = server_banner)
-                )
-            )
+            components.append(discord.ui.MediaGallery(discord.MediaGalleryItem(media = server_banner)))
 
         components.append(
-            discord.ui.TextDisplay(content = footer)
-        )
+            discord.ui.TextDisplay(content = footer))
 
         self.container = discord.ui.Container(*components)
         self.add_item(self.container)
